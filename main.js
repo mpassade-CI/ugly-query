5692b5a3e1f474641ec1b8abb0074fcd51dd81d1
Michael Passade <michael.passade@codeimmersives.com>
2020-02-12 10:36:48 -0500

    done

---
+++ main.js
@@ -0,0 +1,20 @@
+const heading = document.querySelector('h1')
+heading.style.color = 'mediumseagreen'
+heading.style.letterSpacing = '-200px'
+heading.style.fontSize = '500px'
+
+document.body.style.backgroundImage = "url('https://i.ytimg.com/vi/fL3QqgK3UYg/maxresdefault.jpg')";
+
+const par1 = document.querySelector('#p1')
+par1.style.color = 'black'
+par1.style.fontSize = '80px'
+
+const par2 = document.querySelector('#p2')
+par2.style.color = 'gold'
+par2.style.fontWeight = '200'
+par2.style.fontSize = '200px'
+
+const par3 = document.querySelector('#p3')
+par3.style.color = 'gold'
+par3.style.fontWeight = '200'
+par3.style.fontSize = '200px'
